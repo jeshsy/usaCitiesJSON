9f5d23022ee785e0ab80396f798003ab6009aa9b
WizrdJim <wzrdjim@gmail.com>
2017-10-26 14:50:48 -0400

    Made it so that you can import the file and get the results Thanks Jesh!!!

--- usaCitiesJSON.js
+++ usaCitiesJSON.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-
+/*
 fs.readFile('./data/usaCitiesSmall.txt', 'utf8', function(err, data) {  
     if (err) throw err;
     const arr = [];
@@ -27,6 +27,29 @@ fs.readFile('./data/usaCitiesSmall.txt', 'utf8', function(err, data) {
     console.log("The file was saved!");
   }); 
 });
-
+*/
 // console.log(Cities);
 // console.log(arr);
+const usaCityDump = () => {
+    let data = fs.readFileSync(__dirname+'/data/usaCitiesSmall.txt', 'utf8');  
+    const arr = [];
+    let ys = /   /g;
+    let xs =/ /;
+    let name = / \| /;
+    let closebracket = /\r/;
+    data = data.replace(ys, '","y":"');
+    data = data.split('\n');
+    for (let i = 0; i < data.length - 1; i++) {
+      data[i] = data[i].replace(xs, '{"x":"');
+      data[i] = data[i].replace(name, '","name":"');
+      data[i] = data[i].replace(closebracket, '"}');
+      arr.push(JSON.parse(data[i]));
+      // let obj = JSON.parse(data[i]);
+      // arr.push(obj);
+    }
+    // console.log(Cities);
+    //console.log(data);
+  return arr
+}
+
+module.exports = usaCityDump;
